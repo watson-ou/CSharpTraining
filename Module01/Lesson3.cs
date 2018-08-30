69f535b25a42daa3b5eb46bc6ae6ac2a87de96bc
watson-ou <42826634+watson-ou@users.noreply.github.com>
2018-08-30 15:59:56 +1200

    Add files via upload

---
+++ Module01/Lesson3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01
+{
+    // lesson 3 - code reuse/methods
+    class Point
+    {
+        private int x, y, z;
+        // this class represents a 3D point in space
+        void SetLocation(int xValue, int yValue, int zValue)
+        {
+            x = xValue;
+            y = yValue;
+            z = zValue;
+            // return keyword not required
+        } // SetLocation method
+
+        // calculate distance from another point
+        // See, http://charliebravotango.com/link/getlink/951404
+        double Distance(Point p)
+        {
+            double distance = 
+                Math.Sqrt(
+                    Math.Pow(p.x - x, 2)
+                  + Math.Pow(p.y - y, 2)
+                  + Math.Pow(p.z - z, 2));
+            return distance;
+        } // Distance method
+    } // Point class
+} // namespace
---
+++ Module01/VariableScopes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/******************************************************************************
+ * This class demonstrates various scopes for variables
+ * Chris Barker
+ * January 2017
+ * ****************************************************************************/
+
+namespace Module01
+{
+    class VariableScopes
+    {
+        // These variables are scoped to the class
+        int age = 7;
+        string name = "Chris Barker";
+        DateTime created = DateTime.Now;
+
+        void aMethod()
+        {
+            // These variables are scoped to the "aMethod()" method
+            int sum = 0;
+            decimal factor = 0.0M;
+            string message = String.Empty;
+
+            for (int retryCount = 0; retryCount < 3; retryCount++)
+            {
+                // These variables are scoped to the for loop construct
+                // They go out of scope at the closing curly brace - }
+                Random rand = new Random();
+                int randomNumber = rand.Next();
+            } // for
+        } // aMethod
+    } // VariableScopes class
+} // namespace
